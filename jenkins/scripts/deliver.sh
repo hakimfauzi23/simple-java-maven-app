65c71606dae9b8bbf8804ee56a58db6722456d66
Hanif <hanif.hakim@spesolution.com>
2023-12-18 10:08:44 +0700

    Add Background processing #2

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -23,5 +23,5 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/${NAME}-${VERSION}.jar &
 chmod +x ./jenkins/scripts/kill.sh
+java -jar target/${NAME}-${VERSION}.jar &
